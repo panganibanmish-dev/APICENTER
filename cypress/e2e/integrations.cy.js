6bfb01b54de10e137a5da2247a072683f9c2d97c
Michelle Panganiban <panganibanmish.personal@gmail.com>
2023-06-06 19:38:26 +0800

    UPDATED 06/06/23

--- cypress/e2e/integrations.cy.js
+++ cypress/e2e/integrations.cy.js
@@ -5,7 +5,7 @@ describe('Integrations Page Test Suite', () => {
     beforeEach(() => {
         LoginPage.goToLoginPage();
         LoginPage.loginAdmin(Cypress.env('login_email'), Cypress.env('login_password'));
-        cy.get("body").contains("Integrations");
+        cy.get("body").contains("Integrations"); 
         IntegrationsPage.gotoIntegrations();
     });
     it('It should be add two application', () => {
