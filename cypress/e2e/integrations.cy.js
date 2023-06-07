cf0ba8cc5523aeeb8253b007f1992cfe7805e40a
Michelle Panganiban <panganibanmish.personal@gmail.com>
2023-06-07 21:37:43 +0800

    UPDATED 06/07/23

--- cypress/e2e/integrations.cy.js
+++ cypress/e2e/integrations.cy.js
@@ -3,12 +3,29 @@ import LoginPage from "../pages/LoginPage";
 
 describe('Integrations Page Test Suite', () => {
     beforeEach(() => {
-        LoginPage.goToLoginPage();
-        LoginPage.loginAdmin(Cypress.env('login_email'), Cypress.env('login_password'));
-        cy.get("body").contains("Integrations"); 
-        IntegrationsPage.gotoIntegrations();
+        LoginPage
+            .goToLoginPage();
+        LoginPage
+            .loginAdmin(
+                Cypress.env('login_email'),
+                Cypress.env('login_password'));
+        cy.get("body")
+            .contains("Integrations");
+        IntegrationsPage
+            .gotoIntegrations();
     });
-    it('It should be add two application', () => {
-        IntegrationsPage.followStep();
+    it('It should be click the Add Integration button to proceed to step 1', () => {
+        IntegrationsPage
+            .addIntegration();
+        cy.wait(3000);
+    });
+    it('Step 1 - It should showing the label/text and go start to proceed into the step 2 ', () => {
+        IntegrationsPage
+            .followStep1();
+        cy.wait(3000);
+    });
+    it('Step 2 - It should be add two application', () => {
+        IntegrationsPage
+            .followStep2();
     });
 })
--- cypress/pages/IntegrationsPage.js
+++ cypress/pages/IntegrationsPage.js
@@ -15,110 +15,205 @@ class IntegrationsPage {
             cy.get("body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)"),
         btnApplication2: () =>
             cy.get("body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2)"),
-        lblApp: () => cy.get(".px-6.mb-6"),
-        h3App: () => cy.get("h3[class='mx-6 mb-6']"),
-        appStore: () => cy.get("h3[class='mx-6']"),
-        appSearch: () => cy.get("#app-search"),
-        appBox: () => cy.get(".inner-box.overflow-hidden.tile.tile_app"),
-        appConnect: () => cy.get(".px-6.mb-4.justify-center"),
-        appSettings: () => cy.get(".h5"),
+        lblApp: () =>
+            cy.get(".px-6.mb-6"),
+        h3App: () =>
+            cy.get("h3[class='mx-6 mb-6']"),
+        appStore: () =>
+            cy.get("h3[class='mx-6']"),
+        appSearch: () =>
+            cy.get("#app-search"),
+        appBox: () =>
+            cy.get(".inner-box.overflow-hidden.tile.tile_app"),
+        appConnect: () =>
+            cy.get(".px-6.mb-4.justify-center"),
+        appSettings: () =>
+            cy.get(".h5"),
         inputDisplayTitle: () =>
             cy.get("body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)"),
-        inputAPIandClientpassword: () => cy.get("input[type='password']"),
+        inputAPIandClientpassword: () =>
+            cy.get("input[type='password']"),
         inputAPIusername: () =>
             cy.get("body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > input:nth-child(2)"),
         inputAPIurl: () =>
             cy.get("body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)"),
-        btnSave: () => cy.get(".button.button_success"),
-        btnPrevious: () => cy.get("button[role='button']"),
-        btnTestConnection: () => cy.get(".button.button_app"),
+        btnSave: () =>
+            cy.get(".button.button_success"),
+        btnPrevious: () =>
+            cy.get("button[role='button']"),
+        btnTestConnection: () =>
+            cy.get(".button.button_app"),
     };
-
+    //redirect to integration
     gotoIntegrations = () => {
-        this.elements.btnIntegrations().click();
+        this.elements
+            .btnIntegrations()
+            .click();
     };
+    //start add integration
+    addIntegration = () => {
+        this.elements
+            .lblYourIntegrations()
+            .should("be.visible")
+            .contains("Your Integrations");
+        this.elements
+            .btn()
+            .should("be.visible")
+            .contains("Add Integration")
+            .click();
+    };
+    //step 1
+    followStep1 = () => {
+        const label1 = ".h5";
+        const label2 = "div[class='grid-2 wizard_grid'] div p";
 
-    followStep = () => {
-        this.elements.lblYourIntegrations().should("be.visible").contains("Your Integrations");
-        this.elements.btn().should("be.visible").contains("Add Integration").click();
-        this.elements.lblStep()
-            .should("be.visible").contains("Step 1");
-        this.elements.btn()
-            .should("be.visible").contains("Start").click();
-        this.elements.lblStep()
-            .should("be.visible").contains("Step 2 - Connect your apps");
+        this.elements
+            .lblStep()
+            .should("be.visible")
+            .contains("Step 1");
+        cy.get(label1)
+            .should("be.visible")
+            .contains("Follow the steps to complete your integration.");
+        cy.get(label2)
+            .should("be.visible")
+            .contains("In the following steps you can connect your apps and add your settings.");
+        this.elements
+            .btn()
+            .should("be.visible")
+            .contains("Start")
+            .click();
+    };
+    //step 2 add applications
+    followStep2 = () => {
+        this.elements
+            .lblStep()
+            .should("be.visible")
+            .contains("Step 2 - Connect your apps");
         this.connectApplication1();
         this.connectApplication2();
-        this.elements.btnNext().click();
+        this.elements
+            .btnNext()
+            .click();
     };
-
+    //Add Application 1
     connectApplication1 = () => {
-        this.elements.btnApplication1().click();
-        this.elements.lblApp()
-            .should("be.visible").contains("Application 1");
-        this.elements.h3App()
-            .should("be.visible").contains("Your Applications");
-        this.elements.appStore()
-            .should("be.visible").contains("APIcenter appstore");
-        this.elements.appSearch()
-            .should("be.visible").type("Shopify");
+        this.elements
+            .btnApplication1()
+            .click();
+        this.elements
+            .lblApp()
+            .should("be.visible")
+            .contains("Application 1");
+        this.elements
+            .h3App()
+            .should("be.visible")
+            .contains("Your Applications");
+        this.elements
+            .appStore()
+            .should("be.visible")
+            .contains("APIcenter appstore");
+        this.elements
+            .appSearch()
+            .should("be.visible")
+            .type("Shopify");
         cy.wait(2000);
-        this.elements.appBox()
-            .should("be.visible").contains("Shopify").click();
-        this.elements.appConnect()
-            .should("be.visible").contains("Shopify CONNECT");
-        this.elements.appSettings()
-            .should("be.visible").contains("Application Settings");
-        this.elements.inputDisplayTitle()
-            .should("be.visible").type("Shopify");
-        this.elements.inputAPIandClientpassword()
-            .should("be.visible").type("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");
-        this.elements.inputAPIusername()
-            .should("be.visible").type("348193aa4d7df599082bc16454e94054");
-        this.elements.inputAPIurl()
-            .should("be.visible").type("api3000.myshopify.com ");
-        this.elements.btnSave().click();
+        this.elements
+            .appBox()
+            .click();
+        this.elements
+            .appConnect()
+            .should("be.visible")
+            .contains("Shopify CONNECT");
+        this.elements
+            .appSettings()
+            .should("be.visible")
+            .contains("Application Settings");
+        this.elements
+            .inputDisplayTitle()
+            .should("be.visible")
+            .type("Shopify");
+        this.elements
+            .inputAPIandClientpassword()
+            .should("be.visible")
+            .type("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");
+        this.elements
+            .inputAPIusername()
+            .should("be.visible")
+            .type("348193aa4d7df599082bc16454e94054");
+        this.elements
+            .inputAPIurl()
+            .should("be.visible")
+            .type("api3000.myshopify.com ");
+        this.elements
+            .btnSave()
+            .click();
         cy.wait(2000);
-        this.elements.btnTestConnection().click();
+        this.elements
+            .btnTestConnection()
+            .click();
         cy.wait(5000);
-        this.elements.btnSave().click();
     };
-
+    //Add Application 2
     connectApplication2 = () => {
-        const clientID = "body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)"
-        const division = "body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > input:nth-child(2)"
-        const country = "body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)"
+        const clientId = "body > div:nth-child(1) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)"
 
-        this.elements.btnApplication2().click();
-        this.elements.lblApp()
-            .should("be.visible").contains("Application 2");
-        this.elements.h3App()
-            .should("be.visible").contains("Your Applications");
-        this.elements.appStore()
-            .should("be.visible").contains("APIcenter appstore");
-        this.elements.appSearch()
-            .should("be.visible").type("Exact Online");
-        this.elements.appConnect()
-            .should("be.visible").contains("Exact Online CONNECT");
-        this.elements.appSettings()
-            .should("be.visible").contains("Application Settings");
-        this.elements.inputDisplayTitle()
-            .should("be.visible").type("Exact Online");
-        cy.get(clientID)
-            .should("be.visible").type("9305534a-609b-4a7b-ba80-c6eafa034d9c");
-        this.elements.inputAPIandClientpassword()
-            .should("be.visible").type("rQ1oOvz4OYXz");
-        cy.get(country)
-            .should("be.visible").type("US");
-        cy.get(division)
-            .should("be.visible").type("3165647");
-        this.elements.btnSave().click();
+        this.elements
+            .btnApplication2()
+            .click();
+        this.elements
+            .lblApp()
+            .should("be.visible")
+            .contains("Application 2");
+        this.elements
+            .h3App()
+            .should("be.visible")
+            .contains("Your Applications");
+        this.elements
+            .appStore()
+            .should("be.visible")
+            .contains("APIcenter appstore");
+        this.elements
+            .appSearch()
+            .should("be.visible")
+            .type("Shopware 6.4");
+        this.elements
+            .appBox()
+            .should("be.visible")
+            .contains("Shopware 6.4")
+            .click();
+        this.elements
+            .appConnect()
+            .should("be.visible")
+            .contains("Shopware 6.4 CONNECT");
+        this.elements
+            .appSettings()
+            .should("be.visible")
+            .contains("Application Settings");
+        this.elements
+            .inputDisplayTitle()
+            .should("be.visible")
+            .type("Shopware 6.4");
+        cy.get(clientId)
+            .should("be.visible")
+            .type("SWIAUER4EVFSMHDJYJNJEFVOWQ");
+        this.elements
+            .inputAPIandClientpassword()
+            .should("be.visible")
+            .type("Z1NwdkZBTDhMNVRiMHhEejhqZHZUR2JZSFpKNThUcFVaOGNDc3k");
+        this.elements
+            .inputAPIurl()
+            .should("be.visible")
+            .type("https://shopware6.apicenter.dev/public/admin#/login");
+        this.elements
+            .btnSave()
+            .click();
         cy.wait(2000);
-        this.elements.btnTestConnection().click();
+        this.elements
+            .btnTestConnection()
+            .click();
         cy.wait(5000);
-        this.elements.btnSave().click();
+        //step 3 synchronize your applications
     };
-
 }
 
 module.exports = new IntegrationsPage();
